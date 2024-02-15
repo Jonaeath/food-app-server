66d4b572c7f2324028bd426cfbc1bf2d8bf1e706
Jonaeath <jonaeathbcc18@gmail.com>
2024-02-15 20:17:36 +0600

    update for user validation

---
+++ README.md
@@ -0,0 +1,5 @@
+https://www.youtube.com/watch?v=Sonj_Mtmueo&list=PLI0saxAvhd_OdRWyprSe3Mln37H0u4DAp&index=8
+
+7 MERN Project Series | Sign Up & Login page, Express Validator
+
+- npm install express-validator
--- Routes/CreateUser.js
+++ Routes/CreateUser.js
@@ -1,21 +1,34 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/userSchema");
+const { body, validationResult } = require("express-validator");
 
-router.post("/createuser", async (req, res) => {
-  try {
-    const newUser = new User({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-      location: req.body.location,
-    });
-    await newUser.save();
-    res.json({ success: true, user: newUser });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ success: false, error: "Server error" });
+router.post(
+  "/createuser",
+  body("name").notEmpty(),
+  body("email").isEmail(),
+  body("password")
+    .isLength({ min: 5 })
+    .withMessage("Password minimum length should be 5 characters"),
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        location: req.body.location,
+      });
+      await newUser.save();
+      res.json({ success: true, user: newUser });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ success: false, error: "Server error" });
+    }
   }
-});
+);
 
 module.exports = router;
