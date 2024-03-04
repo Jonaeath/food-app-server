89c02f8eeae39f2e8195da426944b9020e3e2e76
Jonaira Manha <abcd@Jonaeath.local>
2024-03-04 11:47:30 +0600

    update code

--- Routes/OrderData.js
+++ Routes/OrderData.js
@@ -5,11 +5,11 @@ const Order = require('../models/orderSchema');
 router.post('/orderData', async (req, res) => {
     let data = req.body.order_data
     await data.splice(0,0,{Order_date:req.body.order_date})
-    console.log("1231242343242354",req.body.email)
+    // console.log("1231242343242354",req.body.email)
 
     //if email not existing in db then create: else: InsertMany()
     let eId = await Order.findOne({ 'email': req.body.email })    
-    console.log(eId)
+    // console.log(eId)
     if (eId===null) {
         try {
             // console.log(data)
@@ -39,5 +39,16 @@ router.post('/orderData', async (req, res) => {
         }
     }
 })
+router.post('/myOrderData', async (req, res) => {
+    try {
+        // console.log(req.body.email)
+        let myData = await Order.findOne({ 'email': req.body.email })
+        //console.log(myData)
+        res.json({orderData:myData})
+    } catch (error) {
+        res.send("Error",error.message)
+    }
+});
+
 
 module.exports = router;
